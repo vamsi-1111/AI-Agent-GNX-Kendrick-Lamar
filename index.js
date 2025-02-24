e9d5e59aeef486f0cd03b43f7433ee88bea60035
Vamsi Chitturi <vch2ri@gmail.com>
2025-02-23 19:24:57 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -7,30 +7,13 @@ async function run()
 {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
-    await page.goto("https://fill.dev/form/credit-card-simple")
-    
-    let selector = "input[id='cc-name']";
+    await page.goto("https://www.amazon.com/GNX-LP-Kendrick-Lamar/dp/B0DP1JZ6DT/ref=asc_df_B0DP1JZ6DT?mcid=42b13374a6373f8aa2a2e0c3bba6bbe2&hvocijid=327381546459992356-B0DP1JZ6DT-&hvexpln=73&tag=hyprod-20&linkCode=df0&hvadid=730434177080&hvpos=&hvnetw=g&hvrand=327381546459992356&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9192197&hvtargid=pla-2281435180738&psc=1")
+
+    let selector = "button[id='nav-assist-add-to-cart']";
     await page.waitForSelector(selector);
-    await page.type(selector, "Vamsi Chitturi");
-
-    selector = "select[id='cc-type']";
-    await page.select(selector, "visa");
-
-    selector = "input[id='cc-number']";
-    await page.type(selector, "1111111111111111");
-
-    selector = "input[id='cc-csc']";
-    await page.type(selector, "111");
-
-    selector = "select[id='cc-exp-month']";
-    await page.select(selector, "11");
-
-    selector = "select[id='cc-exp-year']";
-    await page.select(selector, "2027");
-
-    selector = "button[type='submit']";
-    await page.click(selector);
-
+    await page.evaluate((selector) => {
+        document.querySelector(selector).click();
+    }, selector)
     
 
 }
